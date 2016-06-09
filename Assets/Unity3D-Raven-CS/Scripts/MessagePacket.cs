5a8c9eb4c90c538d2651530e7f553b78f4e8b38b
Ma Tingchen <defmys@gmail.com>
2016-06-09 17:44:58 +0800

    Add MessagePacket

---
+++ Assets/Unity3D-Raven-CS/Scripts/MessagePacket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Unity3DRavenCS
+{
+	[Serializable]
+	public class MessagePacket
+	{
+
+		public string eventID;
+		public string message;
+		public string timestamp;
+		public string level;
+		public string logger;
+		public string platform;
+
+		[Serializable]
+		public struct SDK
+		{
+			public string name;
+			public string version;
+		}
+		public SDK sdk = new SDK();
+
+		[Serializable]
+		public struct Device
+		{
+			public string name;
+			public string version;
+			public string build;
+		}
+		public Device device = new Device();
+
+		public MessagePacket()
+		{
+		}
+
+		public string ToJson()
+		{
+			return JsonUtility.ToJson(this);
+		}
+	}
+}
