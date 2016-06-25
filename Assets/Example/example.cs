d1b791486d853cf0d86e7d99a077dc29ef2a86d3
Ma Tingchen <defmys@gmail.com>
2016-06-25 08:27:34 +0800

    replace the official Json.Net with SaladLab/Json.Net.Unity3D
    The official JSON.NET does not support Unity3D iOS

--- Assets/Example/example.cs
+++ Assets/Example/example.cs
@@ -9,7 +9,7 @@ public class example : MonoBehaviour {
     void Start()
     {
         // Create a raven client with DSN uri.
-        m_client = new Unity3DRavenCS.Unity3DRavenCS("http://5aa275ef70b0416f80a405b258d20a15:801b6e0d9cbf46508560b9ca0b320c6a@192.168.188.128:9000/2");
+		m_client = new Unity3DRavenCS.Unity3DRavenCS("http://ab02bafeb811496c825b4f22631f3ea3:81efdf5ff4f34aeb8e4bb20b27d286eb@192.168.1.109:9000/2");
 
         // Create some tags that need to be sent with log messages.
         m_tags = new Dictionary<string, string>();
@@ -89,8 +89,10 @@ public class example : MonoBehaviour {
         }
     }
 
+	private List<int> l = new List<int>();
     private void ExceptionCall()
     {
+		l = null;
         ExceptionCall1();
     }
 
@@ -101,8 +103,6 @@ public class example : MonoBehaviour {
 
     private void ExceptionCall2()
     {
-        int a = 1;
-        int b = 0;
-        int x = a / b;
+		l.Add(10);
     }
 }
